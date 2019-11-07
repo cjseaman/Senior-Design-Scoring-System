a4618abe34c3fcdcff1896094727880ee5a0f269
Alex Achramowicz <aachramowicz@scu.edu>
2019-11-06 16:32:25 -0800

    Changed judge app to student for demo

--- Django/scoringsystem/session/models.py
+++ Django/scoringsystem/session/models.py
@@ -3,5 +3,6 @@ from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Session(models.Model):
-	sessionDate = models.DateTimeField(auto_now=False, auto_now_add=False)
+	#sessionDate = models.DateTimeField(auto_now=False, auto_now_add=False)
 	addJudge = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,)
+	addStudent = models.ForeignKey('student.Student', on_delete=models.CASCADE)
--- Django/scoringsystem/student/admin.py
+++ Django/scoringsystem/student/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Judge
+from .models import Student
 
-admin.site.register(Judge)
+admin.site.register(Student)
